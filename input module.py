d4b4721e6f1838541b6c2fd02b017472c24ce31e
GanzKrauf <armagydon@gmail.com>
2024-09-01 11:34:11 +0300

    Add files via upload

---
+++ input module.py
@@ -0,0 +1,12 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2nd program"
+print( 9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 +2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+# "4th program"
+str_ = '123.456'
+float_ = float(str_) * 10
+print(int(float_) % 10)
